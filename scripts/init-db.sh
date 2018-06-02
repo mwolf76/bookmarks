64f130fe55d67de066ad32cff49fdfb1f39cd01a
Marco Pensallorto <marco.pensallorto@gmail.com>
2018-06-02 12:03:35 +0200

    Creating db subdirectory if not already there

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -4,6 +4,7 @@ DATABASE_NAME="bookmarks"
 
 if [ ! -d "$DATABASE_DIR" ]; then
 	echo "Initializing db..."
+	mkdir -p "$DATABASE_DIR"
 	DATABASE_URL=jdbc:h2:`pwd`/db/"$DATABASE_NAME" lein migratus
 else
 	echo "db sub-directory exists. Skipping..."
